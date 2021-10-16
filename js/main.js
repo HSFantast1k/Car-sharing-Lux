9af83c10715d00562816ce263f0247763e60cf88
Fantast1k <83510907+HSFantast1k@users.noreply.github.com>
2021-10-16 20:53:27 +0300

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -5,6 +5,20 @@ $(function(){
         autoplay: true,
         autoplaySpeed: 2000,
         fade: true,
+        responsive: [
+            {
+                breakpoint: 1200,
+                settings: {
+                    dots: false,
+                }
+            },
+            // {
+            //     breakpoint: 1000,
+            //     settings: {
+            //         dots: true,
+            //     }
+            // }
+        ]
     });
 
     $('.reviews__slider').slick({
@@ -14,5 +28,30 @@ $(function(){
         dots: true,
         autoplay: true,
         autoplaySpeed: 2000,
+        responsive: [
+            {
+                breakpoint: 1141,
+                settings: {
+                    slidesToShow: 3,
+                }
+            },
+            {
+                breakpoint: 846,
+                settings: {
+                    slidesToShow: 2,
+                }
+            },
+            {
+                breakpoint: 586,
+                settings: {
+                    slidesToShow: 1,
+                }
+            },
+        ]
     })
+
+    $('.menu-btn').on('click', function(){
+        $('.menu__list').slideToggle()
+    })
+
 });
